51208680096acdfb683a585d3a334cff1c39e4a7
Abheesh <143244705+Wraith1057@users.noreply.github.com>
2025-05-01 14:59:34 +0530

    Add files via upload

---
+++ CodeAlpha_task/codealpha_music-player/js/playlist.js
@@ -0,0 +1,26 @@
+let songs = [
+  {
+    name: "Justin Bieber – Ghost",
+    artist: "Justin Bieber",
+    img: "img1",
+    audio: "music1"
+  },
+  {
+    name: "Sunflower - Post Malone",
+    artist: "Post Malone",
+    img: "img2",
+    audio: "music2"
+  },
+  {
+    name: "Blinding lights - The Weeknd",
+    artist: "The Weeknd",
+    img: "img3",
+    audio: "music3"
+  },
+  {
+    name: "Die with a smile - lady gaga,bruno mars",
+    artist: "Lady Gaga, Bruno Mars",
+    img: "img4",
+    audio: "music4"
+  }
+]
